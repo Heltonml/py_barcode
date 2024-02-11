7a67ca7891a621ac5b43498a13c50ccb5be72aa6
Helton <heltonml@gmail.com>
2024-02-11 19:26:46 -0300

    feat: adding barcode tag creator

--- src/views/http_types/tag_creator_view.py
+++ src/views/http_types/tag_creator_view.py
@@ -1,13 +1,14 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpReponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
         responsability for interaction with Http
     '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpReponse:
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body['product_code']
-        print(product_code)
-        # add user_case
-        return HttpReponse(200, body={"result": "success!!!"})
+        response = tag_creator_controller.create(product_code)
+        return HttpReponse(status_code=200, body=response)
