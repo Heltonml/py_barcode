2c29d817415418acae3042e7aa541c118729d0bc
Helton <heltonml@gmail.com>
2024-02-11 20:59:20 -0300

    feat: adding cerberus validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,18 @@
 from src.views.http_types.http_response import HttpReponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntity
 
 def handle_errors(error: Exception) -> HttpReponse:
+    if isinstance(error, HttpUnprocessableEntity):
+        return HttpReponse(
+            status_code=error.status_code,
+            body={
+                "error": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpReponse(
         status_code=500,
         body={
